c33efc23422425af15da9802b5bb3144bc6f92f1
Ben Meyrick <bameyrick@gmail.com>
2023-07-21 00:54:15 +0100

    Change to use MissingTranslationHandlerFn

--- src/translate-module-config.model.ts
+++ src/translate-module-config.model.ts
@@ -1,8 +1,9 @@
+import { MissingTranslationHandlerFn } from '@qntm-code/translation-key-store';
 import { TranslationConfig } from './translation-config.model';
 
 export interface TranslateModuleConfig {
   defaultLanguage: string;
   translations: TranslationConfig[];
   enableMissingTranslationLogging?: boolean;
-  missingTranslationHandler?: (language: string, key: string) => void;
+  missingTranslationHandler?: MissingTranslationHandlerFn;
 }
